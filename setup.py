89e46f6dbcf5f12c0444019b4b54e0635a332ea0
Sudip Bhattarai <sudipbhattarai100@gmail.com>
2026-04-12 15:13:05 +0545

    Release 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="docker-stack",
-    version="0.3.0",
+    version="2.0.1",
     description="CLI for deploying and managing Docker stacks.",
     long_description=open("README.md").read(),  # You can include a README file to describe your package
     long_description_content_type="text/markdown",
